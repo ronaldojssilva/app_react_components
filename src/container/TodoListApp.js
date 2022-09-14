77e0a504b0781c6305bf2b123f74e5700215677f
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-09-14 14:44:09 -0400

    AJAX facil com axios e code spliting

--- src/container/TodoListApp.js
+++ src/container/TodoListApp.js
@@ -1,8 +1,11 @@
 import React, {Component} from "react";
+import axios from "axios";
+
 import TodoForm from "../components/TodoForm";
 import {TodoList} from "../components/TodoList";
 import { ContainerApp } from "../components/ContainerApp";
 import { Message, WelcomeMessage } from "../components/Message";
+import { getTodos } from "../API";
 
 export default class TodoListApp extends Component{
     constructor(props){
@@ -12,12 +15,15 @@ export default class TodoListApp extends Component{
         }
     }
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => response.json())
-        .then(json => {
-           this.setState({items: json}); 
-        });
+    async componentDidMount(){
+        const {data} = await getTodos();
+        this.setState({items: data}); 
+
+        // fetch('https://jsonplaceholder.typicode.com/todos')
+        // .then(response => response.json())
+        // .then(json => {
+        //    this.setState({items: json}); 
+        // });
     }
     pushToItems = (todo) =>{
         const {items} = this.state;
